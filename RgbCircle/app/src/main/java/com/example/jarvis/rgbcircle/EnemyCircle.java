1d24e7250d51754856011a0a1fda1bd4aa948c50
galmax <maks10059@mail.ru>
2016-04-24 22:09:35 +0300

    feature create EnemyCircle.java for display enemy circles

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/EnemyCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/EnemyCircle.java
@@ -1,7 +1,25 @@
 package com.example.jarvis.rgbcircle;
 
+import java.util.Random;
+
 /**
  * Created by Jarvis on 24.04.2016.
  */
-public class EnemyCircle {
+public class EnemyCircle extends SimpleCircle {
+
+    public static final int FROM_RADIUS = 10;
+    public static final int TO_RADIUS = 110;
+
+    public EnemyCircle(int x, int y, int radius) {
+        super(x, y, radius);
+    }
+
+    public static EnemyCircle getRandomCircle() {
+        Random random = new Random();
+        int x = random.nextInt(GameManager.getWidth());
+        int y = random.nextInt(GameManager.getHeight());
+        int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        return enemyCircle;
+    }
 }
