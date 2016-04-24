9be077752e55eae98931540129b20e03cc13500a
galmax <maks10059@mail.ru>
2016-04-24 21:26:09 +0300

    featur add moveMainCircleWhenTouchAt

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/MainCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/MainCircle.java
@@ -5,6 +5,7 @@ package com.example.jarvis.rgbcircle;
  */
 public class MainCircle {
     public static final int INIT_RADIUS = 50;
+    public static final int MAIN_SPEED = 30;
     private int x;
     private int y;
     private int radius;
@@ -25,5 +26,12 @@ public class MainCircle {
 
     public int getX() {
         return x;
+    }
+
+    public void moveMainCircleWhenTouchAt(int x1, int y1) {
+        int dx = (x1 - x) * MAIN_SPEED / GameManager.getWidth();
+        int dy = (y1 - y) * MAIN_SPEED / GameManager.getHeight();
+        x+= dx;
+        y+= dy;
     }
 }
