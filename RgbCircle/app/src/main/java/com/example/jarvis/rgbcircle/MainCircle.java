def215e3f64a6522245eb50fa20c3839b0e0a1ad
galmax <maks10059@mail.ru>
2016-04-24 21:42:49 +0300

    refactor (MainCircle) extends from SimpleCircle.java move parameters to SimpleCircle.java

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/MainCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/MainCircle.java
@@ -3,29 +3,14 @@ package com.example.jarvis.rgbcircle;
 /**
  * Created by Jarvis on 24.04.2016.
  */
-public class MainCircle {
+public class MainCircle extends SimpleCircle {
     public static final int INIT_RADIUS = 50;
     public static final int MAIN_SPEED = 30;
-    private int x;
-    private int y;
-    private int radius;
 
     public MainCircle(int x, int y) {
+        super(x, y, INIT_RADIUS);
         this.x = x;
         this.y = y;
-        radius = INIT_RADIUS;
-    }
-
-    public int getRadius() {
-        return radius;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getX() {
-        return x;
     }
 
     public void moveMainCircleWhenTouchAt(int x1, int y1) {
