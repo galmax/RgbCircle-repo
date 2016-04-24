5a41c828ddbcfa786cac6bb5fa07752a61656912
galmax <maks10059@mail.ru>
2016-04-24 13:34:41 +0300

    Refactor CanvasManager

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
@@ -10,28 +10,15 @@ import android.view.View;
  * Created by Jarvis on 24.04.2016.
  */
 public class CanvasCircle extends View {
-    private MainCircle mainCircle;
-    private Paint paint;
+    GameManager gameManager;
 
     public CanvasCircle(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initMainCircle();
-        initPaint();
-    }
-
-    private void initPaint() {
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
-    }
-
-    private void initMainCircle() {
-        mainCircle = new MainCircle(200, 500);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(mainCircle.getX(), mainCircle.getY(), mainCircle.getRadius(), paint);
+        gameManager.onDraw(canvas);
     }
 }
