61291cf7d7415add5c9c56d995bf6b282b92ce24
galmax <maks10059@mail.ru>
2016-04-24 13:03:10 +0300

    Add onDraw method to display main circle Add initMainCircle and initPaint methods

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
@@ -2,6 +2,7 @@ package com.example.jarvis.rgbcircle;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,12 +10,28 @@ import android.view.View;
  * Created by Jarvis on 24.04.2016.
  */
 public class CanvasCircle extends View {
+    private MainCircle mainCircle;
+    private Paint paint;
+
     public CanvasCircle(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initMainCircle();
+        initPaint();
+    }
+
+    private void initPaint() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    private void initMainCircle() {
+        mainCircle = new MainCircle(200, 500);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.drawCircle(mainCircle.getX(), mainCircle.getY(), mainCircle.getRadius(), paint);
     }
 }
