70770085ea4ceb78ba93fa4fb3f0341e726a84d4
galmax <maks10059@mail.ru>
2016-04-24 15:00:43 +0300

    refactor(CanvasCircle) add initWidthAndHeight method update GameManager constructor add width and height fields

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
@@ -3,17 +3,36 @@ package com.example.jarvis.rgbcircle;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
+
+import java.util.Dictionary;
 
 /**
  * Created by Jarvis on 24.04.2016.
  */
 public class CanvasCircle extends View {
+    private static int width;
+    private static int height;
     GameManager gameManager;
+
 
     public CanvasCircle(Context context, AttributeSet attrs) {
         super(context, attrs);
+        initWidthAndHeight(context);
+        gameManager = new GameManager(this, width, height);
+    }
+
+    private void initWidthAndHeight(Context context) {
+        WindowManager windowManager = (WindowManager) context.getSystemService(context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        width = point.x;
+        height = point.y;
     }
 
     @Override
