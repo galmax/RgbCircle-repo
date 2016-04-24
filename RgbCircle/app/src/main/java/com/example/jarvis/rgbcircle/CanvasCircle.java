71e38c69c88927f181e6fa3b4ed1af380e21be44
galmax <maks10059@mail.ru>
2016-04-24 12:50:50 +0300

    Add CanvasCircle class to display circle view

---
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/CanvasCircle.java
@@ -0,0 +1,20 @@
+package com.example.jarvis.rgbcircle;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Jarvis on 24.04.2016.
+ */
+public class CanvasCircle extends View {
+    public CanvasCircle(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
