274cea7a58710631344fe73549e2390c57697ead
galmax <maks10059@mail.ru>
2016-04-24 20:50:51 +0300

    refactor update GameManager.java move paint object, initPaint method, drawCircle logic to CanvasCircle.java

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/GameManager.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/GameManager.java
@@ -8,30 +8,22 @@ import android.graphics.Paint;
  */
 public class GameManager {
     private MainCircle mainCircle;
-    private Paint paint;
     private CanvasCircle canvasCircle;
     private static int width;
     private static int height;
 
     public GameManager(CanvasCircle canvasCircle, int w, int h) {
+        this.canvasCircle = canvasCircle;
         width = w;
         height = h;
-        this.canvasCircle = canvasCircle;
         initMainCircle();
-        initPaint();
-    }
-
-    private void initPaint() {
-        paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setStyle(Paint.Style.FILL);
     }
 
     private void initMainCircle() {
         mainCircle = new MainCircle(width / 2, height / 2);
     }
 
-    public void onDraw(Canvas canvas) {
-        canvas.drawCircle(mainCircle.getX(), mainCircle.getY(), mainCircle.getRadius(), paint);
+    public void onDraw() {
+        canvasCircle.drawCircle(mainCircle);
     }
 }
