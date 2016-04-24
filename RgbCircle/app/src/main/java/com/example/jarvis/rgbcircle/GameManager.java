3f49d276d0a8546201ffe3b5f178a1a8ac73247a
galmax <maks10059@mail.ru>
2016-04-24 14:58:02 +0300

    refactor (GameManager.java) add width and height of display

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/GameManager.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/GameManager.java
@@ -9,8 +9,14 @@ import android.graphics.Paint;
 public class GameManager {
     private MainCircle mainCircle;
     private Paint paint;
+    private CanvasCircle canvasCircle;
+    private static int width;
+    private static int height;
 
-    public GameManager() {
+    public GameManager(CanvasCircle canvasCircle, int w, int h) {
+        width = w;
+        height = h;
+        this.canvasCircle = canvasCircle;
         initMainCircle();
         initPaint();
     }
@@ -22,7 +28,7 @@ public class GameManager {
     }
 
     private void initMainCircle() {
-        mainCircle = new MainCircle(200, 500);
+        mainCircle = new MainCircle(width / 2, height / 2);
     }
 
     public void onDraw(Canvas canvas) {
