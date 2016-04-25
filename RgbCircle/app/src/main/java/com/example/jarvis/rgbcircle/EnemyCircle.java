aeba61cc232c82914fd216b8b94ab857607dee9c
galmax <maks10059@mail.ru>
2016-04-25 19:08:21 +0300

    chore create moveOneStep for enemy circle, the enemy circles move one step when user touch the display

--- RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/EnemyCircle.java
+++ RgbCircle/app/src/main/java/com/example/jarvis/rgbcircle/EnemyCircle.java
@@ -13,17 +13,25 @@ public class EnemyCircle extends SimpleCircle {
     public static final int TO_RADIUS = 110;
     public static final int ENEMY_COLOR = Color.RED;
     public static final int FOOD_COLOR = Color.rgb(0, 200, 0);
+    public static final int RANDOM_SPEED = 10;
+    private int dx;
+    private int dy;
 
-    public EnemyCircle(int x, int y, int radius) {
+    public EnemyCircle(int x, int y, int radius, int dx, int dy) {
         super(x, y, radius);
+        this.dx = dx;
+        this.dy = dy;
     }
 
     public static EnemyCircle getRandomCircle() {
         Random random = new Random();
         int x = random.nextInt(GameManager.getWidth());
         int y = random.nextInt(GameManager.getHeight());
+        int dx = 1 + random.nextInt(RANDOM_SPEED);
+        int dy = 1 + random.nextInt(RANDOM_SPEED);
+
         int radius = FROM_RADIUS + random.nextInt(TO_RADIUS - FROM_RADIUS);
-        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius);
+        EnemyCircle enemyCircle = new EnemyCircle(x, y, radius , dx, dy);
         return enemyCircle;
     }
 
@@ -40,5 +48,10 @@ public class EnemyCircle extends SimpleCircle {
         if(radius < circle.radius)
             return true;
         return false;
+    }
+
+    public void moveOneStep() {
+        x += dx;
+        y += dy;
     }
 }
